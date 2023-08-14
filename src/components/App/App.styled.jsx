2943197e65d76be09b5a2b4b5682abaadcd882c2
YevheniiaRi <9263070@gmail.com>
2023-08-14 20:56:31 +0300

    fff

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,21 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  max-width: 600px;
+  min-height: 400px;
+  margin-right: auto;
+  margin-left: auto;
+  margin-top: 20px;
+  padding: 10px 15px;
+  outline: 1px solid black;
+
+`;
+
+export const Title = styled.h1`
+  text-align: center;
+`;
+
+export const ContactListTitle = styled.h2`
+  text-align: center;
+  font-size: 28px;
+`;
